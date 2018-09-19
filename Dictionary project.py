208cda52f5594e7beef16f6427200b14a5b7dffe
Madeline Eileen Tatum <madeline.tatum@stu.jefferson.kyschools.us>
2018-09-19 08:28:56 -0400

    Dictionary commit

--- Dictionary project.py
+++ Dictionary project.py
@@ -4,7 +4,7 @@
 cakes = []
 cupcakes = []
 pastries = []
-orders = cakes + cupcakes + pastries
+orders = [cakes,cupcakes,pastries]
 
 order = input("Would you like to place an order?: ")
 
@@ -12,6 +12,12 @@ if order == "no":
     print("Have a nice day!")
 else:
     answer = input("For a cake, use the first function. For a cupcake, use the second function. For a pastry, use the third function. Continue? ")
+    if len(orders) < 1:
+        more = input("Do you have any more orders? ")
+        if more == "no":
+            print("Your order of %s has been placed." % orders)
+        else:
+            pass    # None of this is right and idk how to fix it
     while answer == "yes":
         def first(shape,size,flavor):
             cake = {
@@ -39,8 +45,3 @@ else:
                 }
             pastries.append(pastry)
             return "Thank you for your pastry order."
-        print("You order is %s" % orders)
-
-
-
-    
