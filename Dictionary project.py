48762c41e338a3797dc131a5c20ffcfaeecf07b5
Madeline Eileen Tatum <madeline.tatum@stu.jefferson.kyschools.us>
2018-09-21 08:31:52 -0400

    dictionary commit

--- Dictionary project.py
+++ Dictionary project.py
@@ -18,21 +18,22 @@ def make_customer(cake_id,name):
     return customer
 
 cake_id = 0 
+orders = []
 
 while True:
     why = input("Would you like to order, pickup, or leave? ")
     if why == "order":
         order_name = input("What is the name for the order? ")
-        
+        order_name == make_customer(cake_id,'name')
         cake_id = cake_id + 1
-        order_size = input("What size cake would you like? ")
-        
-        order_shape = input("What shape cake would you like? ")
-        
         order_flavor = input("What flavor cake would you like? ")
-        
+        order_flavor == make_cake(cake_id, shape, size, 'flavor')
+        order_size = input("What size cake would you like? ")
+        order_size == make_cake( cake_id, shape, 'size', flavor)
+        order_shape = input("What shape cake would you like? ")
+        order_shape == make_cake(cake_id,'shape',size,flavor)
     elif why == "pickup":
-        
+        pass
     elif why == "leave":
         print("Have a nice day!")
         break
